6c55499d8126d5421e2fb913a0009364f5cc53f7
M. Saiful Rizal <saifulryzl15@gmail.com>
2018-12-22 08:53:50 +0700

    fix home base

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url']	= 'http://localhost/smk5/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'beranda';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
