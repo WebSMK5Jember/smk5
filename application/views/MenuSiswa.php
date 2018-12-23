905eeeceb7c55dac5b882f5503ceb3702937ba85
M. Saiful Rizal <saifulryzl15@gmail.com>
2018-12-23 21:07:56 +0700

    dipullsayangkuu

--- application/views/MenuSiswa.php
+++ application/views/MenuSiswa.php
@@ -4,11 +4,11 @@
                             <li class="menu-title">List Menu Siswa</li>
                             <li>
                                 <a href="<?php echo base_url(); ?>siswa"><i class="fa fa-home"></i><span> Profil</span></a>
-                                <a href="<?php echo base_url(); ?>siswa/jadwalpelajaran"><i class="fa fa-plus-cirle"></i><span> Jadwal Pelajaran</span></a>
+                                <a href="<?php echo base_url(); ?>siswa/jadwalpelajaran"><i class="fa fa-list"></i><span> Jadwal Pelajaran</span></a>
                                 <a href="<?php echo base_url(); ?>siswa/absensi"><i class="fa fa-list"></i><span> Absensi Siswa</span></a>
                                 <a href="<?php echo base_url(); ?>siswa/nilai"><i class="fa fa-list"></i><span> Nilai Siswa</span></a>
-                                <a href="<?php echo base_url(); ?>siswa/daful"><i class="fa fa-plus-cirle"></i><span> Daftar Ulang</span></a>
-                                <a href="<?php echo base_url(); ?>siswa/prakerin"><i class="fa fa-plus-cirle"></i><span> Daftar Prakerin</span></a>
+                                <a href="<?php echo base_url(); ?>siswa/daful"><i class="fa fa-list"></i><span> Daftar Ulang</span></a>
+                                <a href="<?php echo base_url(); ?>siswa/prakerin"><i class="fa fa-list"></i><span> Daftar Prakerin</span></a>
 
 
                             </li>
