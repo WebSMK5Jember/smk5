fd1b6f971eaff4ab0db0c979584a8d488043e2b4
RERA-PC\RERA <marerapm82@gmail.com>
2018-12-26 18:41:56 +0700

    ngisi guru

--- application/views/MenuGuru.php
+++ application/views/MenuGuru.php
@@ -4,6 +4,10 @@
                             <li class="menu-title">List Menu Guru</li> 
                                 <li>
                                     <a href="<?php echo base_url(); ?>guru"><i class="fa fa-home"></i><span> Beranda</span></a>
+                                    <a href="<?php echo base_url(); ?>guru"><i class="fa fa-home"></i><span> Nilai Siswa</span></a>
+                                    <a href="<?php echo base_url(); ?>guru"><i class="fa fa-home"></i><span> Absensi Siswa</span></a>
+                                    <a href="<?php echo base_url(); ?>guru"><i class="fa fa-home"></i><span> Jadwal dan Ruangan</span></a>
+                                    
                                 </li>
                                    
 
